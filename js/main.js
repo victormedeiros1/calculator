3d7644daf943447633ca69360ae628f553a45590
victormedeiros1 <josevqczt@gmail.com>
2022-07-18 17:39:38 -0300

    feat: color buttons

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll('.calc__button');
+const display = document.querySelector('.calc__display');
+
+for (const button of buttons) {
+  button.addEventListener('click', () => {
+    const value = button.getAttribute('data-value');
+
+    switch (value) {
+      case 'DELETE':
+        display.innerText = ' ';
+    }
+  });
+}
