4c1b69c82f0e363c4161c636866ed6ba92b6f21c
victormedeiros1 <josevqczt@gmail.com>
2022-07-19 15:03:37 -0300

    feat: add font and remake palette colors

--- js/main.js
+++ js/main.js
@@ -4,5 +4,6 @@ const display = document.querySelector('.calc__display');
 for (const button of buttons) {
   button.addEventListener('click', () => {
     const value = button.getAttribute('data-value');
+    console.log(value);
   });
 }
